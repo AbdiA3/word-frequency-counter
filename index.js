8fc87d2d408483e29d2e7de78e0397aa65f928aa
Abdi Adem <abdiadem954@gmail.com>
2021-09-01 21:53:25 +0300

    fix indentation in index.js

--- index.js
+++ index.js
@@ -15,62 +15,62 @@ const program = new Command()
 let filename = null
 
 program
-	.argument('<filename>', 'the path to a text file')
-	.action(f => {
-		filename = f 
-	})
-	.option('-s, --sort', 'sort by words')
-	.option('-p, --print', 'print the frequency table')
-	.option('-sf, --save', "save the frequency table as a JSON file, in '<filename>_freq.json' file")
+    .argument('<filename>', 'the path to a text file')
+    .action(f => {
+        filename = f 
+    })
+    .option('-s, --sort', 'sort by words')
+    .option('-p, --print', 'print the frequency table')
+    .option('-sf, --save', "save the frequency table as a JSON file, in '<filename>_freq.json' file")
 
 program.parse()
 
 let content = null
 
 try {
-	content = readFileSync(filename, 'utf-8')
+    content = readFileSync(filename, 'utf-8')
 
-	const options = program.opts()
-	const words = extract_words(content)
-	let frequency = frequency_counter(words)
+    const options = program.opts()
+    const words = extract_words(content)
+    let frequency = frequency_counter(words)
 
-	if(options.sort) {
-		frequency = sort_object(frequency)
-	}
+    if(options.sort) {
+       frequency = sort_object(frequency)
+    }
 
-	if(options.print) {
-		console.log(chalk.bold.blue(`There are ${return_array(frequency).length} words in total in ${filename}.`))
-		console.table(frequency)
-	}
+    if(options.print) {
+        console.log(chalk.bold.blue(`There are ${return_array(frequency).length} words in total in ${filename}.`))
+        console.table(frequency)
+    }
 
-	if(options.save) {
-		writeFileSync(`${filename}_freq.json`, JSON.stringify([frequency], null, 4))
-		console.log(chalk.bold.blue(`Frequency table for ${filename} is written in ${filename}_freq.json`))
-	}
+    if(options.save) {
+        writeFileSync(`${filename}_freq.json`, JSON.stringify([frequency], null, 4))
+        console.log(chalk.bold.blue(`Frequency table for ${filename} is written in ${filename}_freq.json`))
+    }
 
 } catch(err) {
-	console.log(chalk.bold.red(err))
+    console.log(chalk.bold.red(err))
 }
 
 /**
  *  Main function that computes the frequency for the given words array, 
  *  and return an object containing all the words with their respective frequency
  *  @param {Object[]} words - An array of words
- * 	@returns {Object} - An Object containing the frequency for all the words
+ *  @returns {Object} - An Object containing the frequency for all the words
  * 
  */
 function frequency_counter(words) {
-	let frequency = {}
+    let frequency = {}
 
-	for(let i of words) {
-		if(frequency[i]) {
-			frequency[i]++
-		} else {
-			frequency[i] = 1
-		}
-	}
+    for(let i of words) {
+        if(frequency[i]) {
+            frequency[i]++
+        } else {
+            frequency[i] = 1
+        }
+    }
 
-	return frequency
+    return frequency
 }
 
 /**
@@ -81,32 +81,32 @@ function frequency_counter(words) {
  * 
  */
 function extract_words(text) {
-	const whitespace = ['\t', '\n', '\r', '\x0b', '\x0c', ' ']
-	const punctuation = ['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~']
-	let words = []
-	let temp = text
+    const whitespace = ['\t', '\n', '\r', '\x0b', '\x0c', ' ']
+    const punctuation = ['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~']
+    let words = []
+    let temp = text
 
-	for(let i of whitespace) {
-		temp = temp.split(i).join('|')
-	}
+    for(let i of whitespace) {
+        temp = temp.split(i).join('|')
+    }
 
-	temp = temp.split('|').filter(x => x != '')
-	
-	for(let j=0; j<punctuation.length; j++) {
-		for(let i=0; i<temp.length; i++) {
-			temp[i] = replace_all(temp[i], punctuation[j])
-		}
-	}
+    temp = temp.split('|').filter(x => x != '')
+    
+    for(let j=0; j<punctuation.length; j++) {
+        for(let i=0; i<temp.length; i++) {
+            temp[i] = replace_all(temp[i], punctuation[j])
+        }
+    }
 
-	temp = temp.filter(x => x != '' && x != ' ')
+    temp = temp.filter(x => x != '' && x != ' ')
 
-	for(let i of temp) {
-		words  = [ ...words, ...i.split(' ') ]
-	}
+    for(let i of temp) {
+        words  = [ ...words, ...i.split(' ') ]
+    }
 
-	words = words.filter(x => x != '')
+    words = words.filter(x => x != '')
 
-	return to_lower(words)
+    return to_lower(words)
 }
 
 /**
@@ -118,11 +118,11 @@ function extract_words(text) {
  * 
  */
 function replace_all(string, search_value) {
-	while(string.includes(search_value)) {
-		string = string.replace(search_value, ' ')
-	}
+    while(string.includes(search_value)) {
+        string = string.replace(search_value, ' ')
+    }
 
-	return string
+    return string
 }
 
 /**
@@ -132,13 +132,13 @@ function replace_all(string, search_value) {
  * 
  */
 function to_lower(array) {
-	let new_array = []
+    let new_array = []
 
-	for(let i of array) {
-		new_array.push(i.toLowerCase())
-	}
+    for(let i of array) {
+        new_array.push(i.toLowerCase())
+    }
 
-	return new_array
+    return new_array
 }
 
 /**
@@ -148,13 +148,13 @@ function to_lower(array) {
  * 
  */
 function return_array(obj) {
-	let intermediate_array = []
+    let intermediate_array = []
 
-	for(let i in obj) {
-		intermediate_array.push([i, obj[i]])
-	}
+    for(let i in obj) {
+        intermediate_array.push([i, obj[i]])
+    }
 
-	return intermediate_array
+    return intermediate_array
 }
 
 /**
@@ -164,14 +164,14 @@ function return_array(obj) {
  * 
  */
 function sort_object(obj) {
-	let new_object = {}
-	let intermediate_array = return_array(obj)
+    let new_object = {}
+    let intermediate_array = return_array(obj)
 
-	intermediate_array.sort()
+    intermediate_array.sort()
 
-	for(let i of intermediate_array) {
-		new_object[i[0]] = i[1]
-	}
+    for(let i of intermediate_array) {
+        new_object[i[0]] = i[1]
+    }
 
-	return new_object
+    return new_object
 }
